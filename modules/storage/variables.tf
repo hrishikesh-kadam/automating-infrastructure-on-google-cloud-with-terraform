941c418822ca38acab8fad99b1ebb3c77ca00714
Hrishikesh Kadam <hrkadam.92@gmail.com>
2022-04-04 20:47:11 +0000

    Task 1

---
+++ modules/storage/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "Default Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Default Zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "project_id" {
+  description = "Project ID"
+  type        = string
+  default     = "qwiklabs-gcp-01-2ce18408c671"
+}
