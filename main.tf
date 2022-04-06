b3896e8a813b7bdbdfc270f68e24980416873e9d
Hrishikesh Kadam <hrkadam.92@gmail.com>
2022-04-06 15:17:36 +0530

    Task 6a

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.15.0"
+      version = "~> 3.45"
     }
   }
 }
@@ -21,6 +21,25 @@ module "instances" {
 module "storage" {
   source         = "./modules/storage"
   tf_bucket_name = var.tf_bucket_name
+}
+
+module "tf-vpc" {
+  source       = "terraform-google-modules/network/google"
+  version      = "3.4.0"
+  network_name = "tf-vpc-987869"
+  project_id   = var.project_id
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = var.region
+    },
+    {
+      subnet_name   = "subnet-02"
+      subnet_ip     = "10.10.20.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
 
 terraform {
